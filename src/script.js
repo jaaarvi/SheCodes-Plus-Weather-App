72fbfae00d484cfce7e426309bcab66a34292d90
jarvi <76447093+jaaarvi@users.noreply.github.com>
2021-02-15 17:03:40 -0600

    Added search functionality and forecast data

--- src/script.js
+++ src/script.js
@@ -12,15 +12,60 @@ function updateWeather(response) {
                   />`;
   let feelsLike = document.querySelector("#feels-like");
   feelsLike.innerHTML = Math.round(response.data.main.feels_like);
-  let currentPress = document.querySelector("#pressure");
-  currentPress.innerHTML = response.data.main.pressure;
   let currentHumid = document.querySelector("#humid");
   currentHumid.innerHTML = response.data.main.humidity;
   let currentWind = document.querySelector("#wind");
   currentWind.innerHTML = Math.round(response.data.wind.speed);
 
+  console.log(response.data);
   showLocation(response.data.name);
   getTime(response.data.dt);
+  getForecast(response.data.coord.lat, response.data.coord.lon);
+}
+
+//Update forecast interface
+function updateForecast(response) {
+  let forecast = null;
+  let date = null;
+  let days = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"]
+  let day = null;
+  let forecastDisplay = document.querySelector(".forecast");
+
+  for (let index = 0; index < 5; index++) {
+  forecast = response.data.daily[index];
+  date = new Date(forecast.dt * 1000);
+  day = days[date.getDay()];
+  forecastDisplay.innerHTML += `<div class="row" id="forecast-row">
+                              <div class="col">
+                                <span class="forecast-day">${day}</span>
+                              </div>
+                              <div class="col">
+                                <img
+                                  src="https://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png"
+                                  alt="${forecast.weather[0].description}"
+                                  class="forecast-icon"
+                                />
+                              </div>
+                              <div class="col"><span class="forecast-temp">
+                                high ${Math.round(forecast.temp.max)}°<br />
+                                low ${Math.round(forecast.temp.min)}</span>
+                              </div>
+                            </div>`;
+  }
+
+}
+
+//Get forecast data
+function getForecast(latitude, longitude) {
+  let apiKey = "841177f590ddad9bbbcdad145d970953";
+  let lat = latitude;
+  let lon = longitude;
+  let apiEndpoint = "https://api.openweathermap.org/data/2.5/onecall";
+  let units = "metric";
+
+  let apiUrl = `${apiEndpoint}?lat=${lat}&lon=${lon}&units=${units}&appid=${apiKey}`;
+
+  axios.get(apiUrl).then(updateForecast);
 }
 
 //Update time
@@ -42,10 +87,6 @@ function showLocation(val) {
   location.innerHTML = val;
 }
 
-//Search form function
-
-//Geolocation button function
-
 //Get weather by city value
 function getWeather(val) {
   let apiKey = "841177f590ddad9bbbcdad145d970953";
@@ -58,4 +99,23 @@ function getWeather(val) {
   axios.get(apiUrl).then(updateWeather);
 }
 
+//Search form function
+function searchLocation(event) {
+  event.preventDefault();
+
+  let newLocation = document.querySelector("#search-input");
+  let city = newLocation.value;
+
+  getWeather(city);
+}
+
+let searchInput = document.querySelector("#search-form");
+searchInput.addEventListener("submit", searchLocation);
+let searchButton = document.querySelector("#search-button");
+searchButton.addEventListener("click", searchLocation);
+
+//Geolocation button function
+
+
+//Intial load in
 getWeather("Chicago");
